eb9bac5e9d21f67905ea4ffc40a520f7dd41aea2
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-28 05:31:46 +0300

    Проверка на наличие всех 26 ЖКХ проблем в списке

---
+++ src/test/java/ru/open/kzn/autotests/SendOrderPositiveProblemsListElements.java
@@ -0,0 +1,27 @@
+package ru.open.kzn.autotests;
+
+import com.codeborne.selenide.junit.ScreenShooter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SendOrderPositiveProblemsListElements {
+
+    @Rule
+    public ScreenShooter screenShooter = ScreenShooter.failedTests();
+
+    @Before
+    public void SendOrderOpen() {
+        open("https://open.kzn.ru/sendorder");
+    }
+
+    @Test
+    public void ProblemListHasAllElements() {
+
+        $$(By.xpath("//[@id='selectProblemsObject']//[@class='listOption']//*[@class='option']//*[text()]")).shouldHaveSize(26);
+    }
+}

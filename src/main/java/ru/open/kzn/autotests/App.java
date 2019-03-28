27497928db22dfed3bb550480c67cb5deaa5bf1c
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-28 04:14:11 +0300

    Some date experiments

--- src/main/java/ru/open/kzn/autotests/App.java
+++ src/main/java/ru/open/kzn/autotests/App.java
@@ -1,5 +1,10 @@
 package ru.open.kzn.autotests;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+        DateTimeFormatter month = DateTimeFormatter.ofPattern( "MM" );
+        LocalDateTime todayDate = LocalDateTime.now();
+        String todayMonth = todayDate.format(month);
+        int todayMonthDigit = Integer.parseInt(todayMonth);
+
+        List<Integer> monthsWithHeat = Arrays.asList(10, 11, 12, 1, 2, 3);
+
+        if (monthsWithHeat.contains(todayMonthDigit)) {
+            System.out.println("Account found");
+        }
+
+
     }
 }

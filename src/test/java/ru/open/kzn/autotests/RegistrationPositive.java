d3819cb925d8bd46cbc82e904cad9045a9b70736
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-30 19:51:22 +0300

    Достаю пароль из Gmail

--- src/test/java/ru/open/kzn/autotests/RegistrationPositive.java
+++ src/test/java/ru/open/kzn/autotests/RegistrationPositive.java
@@ -1,5 +1,6 @@
 package ru.open.kzn.autotests;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.junit.ScreenShooter;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,11 +11,11 @@ import org.openqa.selenium.By;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byAttribute;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.url;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
 
 public class RegistrationPositive {
 
@@ -30,14 +31,33 @@ public class RegistrationPositive {
     @Test
     public void RegistrationPositiveNewUser() {
 
+        String registeredEmail = "open.kzn.registered@gmail.com";
+        String registeredEmailPassword = "236-4-123";
         DateTimeFormatter timestamp = DateTimeFormatter.ofPattern( "HH.mm.dd.MM.yyyy" );
         LocalDateTime currentDayTime = LocalDateTime.now();
         String timestampEmail = currentDayTime.format(timestamp);
         timestampEmail += "@lagunov.pro";
+//
+//        $("#auth").find(byAttribute("data-ui","registration")).click();
+//
+        clearBrowserCache();
+        open("https://mail.google.com/mail/h/1pq68r75kzvdr/?v%3Dlui");
+        $("#identifierId").setValue(registeredEmail).pressEnter();
+        $("#password input").setValue(registeredEmailPassword).pressEnter();
+        $$(By.className("ts")).get(1).click();
+        $(By.linkText(timestampEmail));
+        String emailSource = source();
+        String[] array1 = emailSource.split("( </span>)");
+        String emailSourcePasswordPart = array1[1];
+        int length = emailSourcePasswordPart.length();
+        String password = emailSourcePasswordPart.substring(length - 7, length);
+        System.out.println(password);
 
-        $("#auth").find(byAttribute("data-ui","registration")).click();
 
-        $(By.name("email")).setValue(timestampEmail);
+
+
+
+//        $(By.name("email")).setValue(timestampEmail);
 //        String registeredUserLogin = "open.kzn.registered@gmail.com";
 //        String registeredUserPassword = "q6108r";
 //        $(By.name("password")).setValue(rot13(registeredUserPassword)).pressEnter();

4cfcf08fe2d8137dd5da9c437a4a1a89129ab0b2
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-30 16:16:21 +0300

    Два негативных сценария входа

---
+++ src/test/java/ru/open/kzn/autotests/LoginNegative.java
@@ -0,0 +1,54 @@
+package ru.open.kzn.autotests;
+
+import com.codeborne.selenide.junit.ScreenShooter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.openqa.selenium.By.id;
+
+public class LoginNegative {
+
+    @Rule
+    public ScreenShooter screenShooter = ScreenShooter.failedTests();
+
+    @Before
+    public void SendOrderOpen() {
+        open("https://open.kzn.ru/");
+    }
+
+    @Test
+    public void LoginNegativeUnregisteredUser() {
+
+        String registeredUserLogin = "open.kzn.registered@gmail.com";
+        String registeredUserPassword = "q6108r";
+        $("#auth").find(byAttribute("data-ui","auth")).click();
+        $(By.name("username")).setValue(registeredUserLogin);
+        $(By.name("password")).setValue(registeredUserPassword).pressEnter();
+        
+        $(("#js_nofify")).shouldHave(text("Ошибка входа в личный кабинет"));
+
+    }
+
+    @Test
+    public void LoginNegativeRegisteredUserWrongPassword() {
+
+        String unregisteredUserLogin = "open.kzn.unregistered@gmail.com";
+        String unregisteredUserPassword = "wrongpass";
+        $("#auth").find(byAttribute("data-ui","auth")).click();
+        $(By.name("username")).setValue("open.kzn.registered@gmail.com");
+        $(By.name("password")).setValue(unregisteredUserPassword).pressEnter();
+
+        $(("#js_nofify")).shouldHave(text("Ошибка входа в личный кабинет"));
+
+    }
+
+
+}

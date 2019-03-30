92650ce657efc7664f103ded6056889ade0f8235
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-30 14:36:57 +0300

    Добавил перекодировку пароля. Всё равно не очень безопасно, но всё же.

--- src/test/java/ru/open/kzn/autotests/LoginPositiveRegistrationNewUser.java
+++ src/test/java/ru/open/kzn/autotests/LoginPositiveRegistrationNewUser.java
@@ -14,7 +14,6 @@ import static org.openqa.selenium.By.*;
 
 public class LoginPositiveRegistrationNewUser {
 
-
     @Rule
     public ScreenShooter screenShooter = ScreenShooter.failedTests();
 
@@ -28,9 +27,22 @@ public class LoginPositiveRegistrationNewUser {
 
         $("#auth").find(byAttribute("data-ui","auth")).click();
         $(By.name("username")).setValue("open.kzn.registered@gmail.com");
-        $(By.name("password")).setValue("236-4-123").pressEnter();
+        String registeredUserPassword = "q6108r";
+        $(By.name("password")).setValue(rot13(registeredUserPassword)).pressEnter();
         $(id("js_nofify")).shouldHave(text("Ошибка входа в личный кабинет"));
 
     }
 
+    private static String rot13(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }

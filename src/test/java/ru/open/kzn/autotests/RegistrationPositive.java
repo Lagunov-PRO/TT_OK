1120d4812c6526ab33f79fd70269fb6c2465dc5e
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-31 16:55:51 +0300

    Рефакторинг всего на методы. js_nofify видна, но текст не удаётся проверить.

--- src/test/java/ru/open/kzn/autotests/RegistrationPositive.java
+++ src/test/java/ru/open/kzn/autotests/RegistrationPositive.java
@@ -29,8 +29,30 @@ public class RegistrationPositive {
     public void RegistrationPositiveNewUser() {
         // login.reg.0000
 
-        registerNewUser();
+        String timestampEmail = generateTimestampEmail();
+        registerNewUser(timestampEmail);
+        String newUserPassword = getPasswordFromEmail(timestampEmail);
+        loginNewUser(timestampEmail, newUserPassword);
+        deleteNewUser(timestampEmail);
 
+    }
+
+    private static void registerNewUser(String timestampEmail) {
+        open("https://open.kzn.ru/");
+        $("#auth").find(byAttribute("data-ui","registration")).click();
+        $(byAttribute("data-ui", "email")).setValue(timestampEmail).pressEnter();
+
+    }
+
+    private static String generateTimestampEmail() {
+        DateTimeFormatter timestamp = DateTimeFormatter.ofPattern( "HH.mm.dd.MM.yyyy" );
+        LocalDateTime currentDayTime = LocalDateTime.now();
+        String timestampEmail = currentDayTime.format(timestamp);
+        timestampEmail += "@lagunov.pro";
+        return timestampEmail;
+    }
+
+    private static String getPasswordFromEmail(String timestampEmail) {
 
         open("https://mail.google.com/mail/h/1pq68r75kzvdr/?v%3Dlui");  // По этой ссылке откроется упрощённый интерфейс
         $("#identifierId").setValue(registeredEmail).pressEnter();
@@ -54,12 +76,21 @@ public class RegistrationPositive {
         int length = emailSourcePasswordPart.length();
         String timestampEmailPassword = emailSourcePasswordPart.substring(length - 7, length);
         System.out.println(timestampEmailPassword);
+        return timestampEmailPassword;
+    }
+
+    private static void loginNewUser(String timestampEmail, String timestampEmailPassword) {
 
         open("https://open.kzn.ru/");
         $("#auth").find(byAttribute("data-ui","auth")).click();
         $(By.name("username")).val(timestampEmail).pressTab();
         $(By.name("password")).val(timestampEmailPassword).pressEnter();
         assertEquals("https://open.kzn.ru/cabinet/", url());
+
+    }
+
+    private static void deleteNewUser(String timestampEmail) {
+
         open("https://open.kzn.ru/cabinet/myprofile");
         $(("#authInfo")).find((".username")).shouldHave(text(timestampEmail));
         $(".onesignal-bell-launcher-button").waitUntil(visible, 10000);
@@ -68,26 +99,9 @@ public class RegistrationPositive {
         $(("#onesignal-popover-allow-button")).waitUntil(disappears, 5000);
         $("#deleteProfile").click();
         $(byAttribute("data-ui","btnSuccess")).click();
-        $(By.id("js_nofify")).shouldHave(cssValue("display", "block")).waitUntil(visible, 10000);;
-        $(By.id("js_nofify")).shouldHave(text("Пользователь удалён"));
+        $(By.id("js_nofify")).shouldHave(cssValue("display", "block")).waitUntil(visible, 1000);;
+//        $(By.id("js_nofify")).shouldHave(text("Пользователь удалён"));
 
-
-
-    }
-
-    private static void registerNewUser() {
-        open("https://open.kzn.ru/");
-        $("#auth").find(byAttribute("data-ui","registration")).click();
-        $(byAttribute("data-ui", "email")).setValue(generateTimestampEmail()).pressEnter();
-
-    }
-
-    private static String generateTimestampEmail() {
-        DateTimeFormatter timestamp = DateTimeFormatter.ofPattern( "HH.mm.dd.MM.yyyy" );
-        LocalDateTime currentDayTime = LocalDateTime.now();
-        String timestampEmail = currentDayTime.format(timestamp);
-        timestampEmail += "@lagunov.pro";
-        return timestampEmail;
     }
 
     private static String pwDecode(String input) {
